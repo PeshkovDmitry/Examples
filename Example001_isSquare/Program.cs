b8c9e244beaa44e84fd1c3f0a2935e64a6fbf6ab
Peshkov Dmitry <peshkov-dv@yandex.ru>
2023-03-05 15:01:07 +0300

    Adding Example002

---
+++ Example001_isSquare/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n2 == n1*n1) {
+    Console.WriteLine("Является");
+}
+else 
+{
+    Console.WriteLine("Не является");
+}
